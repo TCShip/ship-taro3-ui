93b1a6ece0536026304d40069bb30a5a79f6bbd3
paul <dongpoding@gmail.com>
2021-04-22 09:40:44 +0800

    去除 AtNoticebar animation动画样式

--- src/components/AtNoticebar/AtNoticebar.tsx
+++ src/components/AtNoticebar/AtNoticebar.tsx
@@ -42,73 +42,12 @@ export default class AtNoticebar extends AtComponent<AtNoticeBarProps, AtNoticeB
       public componentWillReceiveProps (): void {
         if (!this.timeout) {
           this.interval && clearInterval(this.interval)
-          this.initAnimation()
         }
       }
     
       public componentDidMount (): void {
         if (!this.props.marquee) return
-        this.initAnimation()
       }
-    
-      private initAnimation (): void {
-        const {
-          isWEAPP,
-          isALIPAY,
-        } = this.state
-        this.timeout = setTimeout(() => {
-          this.timeout = null
-          if (this.state.isWEB) {
-            const elem = document.querySelector(`.${this.state.animElemId}`)
-            if (!elem) return
-            const width = elem.getBoundingClientRect().width
-            const dura = width / (+this.props.speed!)
-            this.setState({ dura })
-          } else if (isWEAPP || isALIPAY) {
-            const query = Taro.createSelectorQuery()
-            query.select(`.${this.state.animElemId}`).boundingClientRect().exec(res => {
-              const queryRes = res[0]
-              if (!queryRes) return
-              const { width } = queryRes
-              const dura = width / (+this.props.speed!)
-              const animation = Taro.createAnimation({
-                duration: dura * 1000,
-                timingFunction: 'linear',
-              })
-              const resetAnimation = Taro.createAnimation({
-                duration: 0,
-                timingFunction: 'linear',
-              })
-              const resetOpacityAnimation = Taro.createAnimation({
-                duration: 0,
-                timingFunction: 'linear',
-              })
-              const animBody = () => {
-                resetOpacityAnimation.opacity(0).step()
-                this.setState({ animationData: resetOpacityAnimation.export() })
-    
-                setTimeout(() => {
-                  resetAnimation.translateX(0).step()
-                  this.setState({ animationData: resetAnimation.export() })
-                }, 300)
-    
-                setTimeout(() => {
-                  resetOpacityAnimation.opacity(1).step()
-                  this.setState({ animationData: resetOpacityAnimation.export() })
-                }, 600)
-    
-                setTimeout(() => {
-                  animation.translateX(-width).step()
-                  this.setState({ animationData: animation.export() })
-                }, 900)
-              }
-              animBody()
-              this.interval = setInterval(animBody, (dura * 1000) + 1000)
-            })
-          }
-        }, 100)
-      }
-    
 
     public render(): JSX.Element {
         const {
@@ -163,7 +102,7 @@ export default class AtNoticebar extends AtComponent<AtNoticeBarProps, AtNoticeB
                     </View>
                 )}
                 <View className='at-noticebar__content-text'>
-                    <View animation={this.state.animationData} className={classNames(innerClassName)} style={style}>{this.props.children}</View>
+                    <View className={classNames(innerClassName)} style={style}>{this.props.children}</View>
                 </View>
                 </View>
                 {showMore && (
